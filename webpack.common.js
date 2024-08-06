6df2a1a206bd5553fa8f7549361c788b93bc3779
jemnset <jemn.set@gmail.com>
2024-08-06 17:04:11 -0400

    Created different config files for running on webpack-dev-server (dev config) and creating a build (prod config)

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    watchFiles: './src/index.html',
   },
   plugins: [
     new HtmlWebpackPlugin({
